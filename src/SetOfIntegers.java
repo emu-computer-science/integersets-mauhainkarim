e893ef9a64018c80c5162cb4d60b0659ab1de77b
Colin Maurier <colinmauriertech@gmail.com>
2021-10-18 18:54:54 -0400

    cleaned up add method. updated main.

--- src/SetOfIntegers.java
+++ src/SetOfIntegers.java
@@ -15,7 +15,8 @@ public class SetOfIntegers {
 		int currentInt;
 
 		System.out.println("Please enter a sequence of integers separated by spaces.");
-		System.out.println("Terminate the sequence by pressing ENTER.");
+		System.out.println("Terminate the sequence by pressing ENTER (or return),\n" +
+				"then typing 'Q'. ");
 
 		// Read in sequence of integers to set
 		token = input.next();
@@ -26,14 +27,15 @@ public class SetOfIntegers {
 		}
 
 		System.out.println("Sequence terminated.");
-
+		System.out.println("Your Integer List is now: ");
 		// Print new set
-		for (int i = 0; i < set.size() - 1; i++) {
+		for (int i = 0; i < set.size(); i++) {
 			if (i == 0)
 				System.out.print(set.get(i));
 			else
 				System.out.print(", " + set.get(i));
 		}
+		System.out.println();
 	}
 	
 	public char getID() {
