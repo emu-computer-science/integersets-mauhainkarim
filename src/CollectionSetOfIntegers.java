866e7564c124220f642291dae121604c5a654939
Colin Maurier <colinmauriertech@gmail.com>
2021-10-18 21:50:41 -0400

    updated save.

--- src/CollectionSetOfIntegers.java
+++ src/CollectionSetOfIntegers.java
@@ -2,17 +2,18 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class CollectionSetOfIntegers implements Serializable{
+public class CollectionSetOfIntegers implements Serializable {
 	// Create collection of SetOfIntegers type, as well as a char array of the alphabet for naming the sets
 	ArrayList<SetOfIntegers> collection = new ArrayList<SetOfIntegers>();
 	char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+	private Object CollectionSetOfIntegers;
 
 	public void add() {
 		int size = collection.size();
 		SetOfIntegers set = new SetOfIntegers(alphabet[size]);
 		collection.add(set);
 	}
-	
+
 	public void delete(int index) {
 		if (index == 0)
 			System.out.println("\nCannot delete because there is no currently selected set.");
@@ -20,10 +21,9 @@ public class CollectionSetOfIntegers implements Serializable{
 			collection.remove(index - 65);
 			System.out.println("\nSet " + alphabet[index - 65] + " has been deleted. Remaining sets have been relabeled.");
 			update();
-			if(index - 65 > getSize() || getSize() == 0) {
+			if (index - 65 > getSize() || getSize() == 0) {
 				System.out.println("No set is currently selected.");
-			}
-			else {
+			} else {
 				System.out.print("The current set is " + collection.get(index - 65).getID() + ". [" + collection.get(index - 65).getState() + "] {");
 				// Print new set
 				for (int i = 0; i < collection.get(index - 65).getSize(); i++) {
@@ -36,35 +36,35 @@ public class CollectionSetOfIntegers implements Serializable{
 			}
 		}
 	}
-	
-	public static void save() {
+
+	public void save() {
 		Scanner filename = new Scanner(System.in);
 		System.out.println("Please enter a file name to save the state of this system under: (ex: 'file.txt').");
 		String N = filename.nextLine();
 		try {
 			ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(N));
+			objectOutputStream.writeObject(CollectionSetOfIntegers);
 		} catch (IOException e) {
 			System.out.println("Unable to write to file " + N + ".");
 		}
 
 
 	}
-	
+
 	public void restore() {
-	   System.out.println("Not yet implemented");
+		System.out.println("Not yet implemented");
 	}
-	
+
 	public boolean quit() {
-	   return false;
+		return false;
 	}
-	
+
 	public void update() {
-		for(int i = 0; i < collection.size(); i++)
+		for (int i = 0; i < collection.size(); i++)
 			collection.get(i).setID(alphabet[i]);
 	}
-	
+
 	public int getSize() {
 		return collection.size();
 	}
-	
 }
